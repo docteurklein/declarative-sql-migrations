70273f8b32e276b079da658e0357b361b074ae68
Florian Klein <florian.klein@free.fr>
2021-11-22 09:37:36 +0100

    work on a set of alterations

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ A postgres migration tool based on diffing 2 schemata.
 ## how?
 
     psql -f diff.sql -c \
-    "call pgdiff.migrate('desired', 'actual',
+    "call pgdiff.migrate('desired', 'target',
         dry_run => true,
         keep_extra => false
     )"
--- diff.sql
+++ diff.sql
@@ -5,7 +5,7 @@ create schema pgdiff;
 
 \set search_path to pgdiff;
 
-create function pgdiff.ddl(ddl text, dry_run bool default true) returns void
+create function pgdiff.ddl(inout ddl text, dry_run bool default true) returns text
 language plpgsql as $$
 begin
     raise notice '%', ddl;
@@ -15,12 +15,22 @@ begin
 end
 $$;
 
-create procedure pgdiff.migrate(
-    desired text,
-    target text,
-    dry_run bool default true,
-    keep_extra bool default false
-)
+drop type if exists command cascade;
+create type command as enum (
+    'add_table',
+    'add_column',
+    'alter_column',
+    'drop_table',
+    'drop_column'
+);
+
+drop type if exists alteration cascade;
+create type alteration as (
+    ddl text,
+    command command
+);
+
+create function pgdiff.alterations(desired text, target text) returns setof alteration
 language plpgsql as $$
 declare
     missing_table record;
@@ -38,20 +48,18 @@ begin
         select table_name from information_schema.tables
         where (table_schema, table_type) = (target, 'BASE TABLE')
     loop
-        perform pgdiff.ddl(format('create table %I.%I ()', target, missing_table.table_name), dry_run);
+        return next row(format('create table %I.%I ()', target, missing_table.table_name), 'add_table')::alteration;
     end loop;
 
-    if not keep_extra then
-        for missing_table in
-            select table_name from information_schema.tables
-            where (table_schema, table_type) = (target, 'BASE TABLE')
-            except
-            select table_name from information_schema.tables
-            where (table_schema, table_type) = (desired, 'BASE TABLE')
-        loop
-            perform pgdiff.ddl(format('drop table %I.%I', target, missing_table.table_name), dry_run);
-        end loop;
-    end if;
+    for missing_table in
+        select table_name from information_schema.tables
+        where (table_schema, table_type) = (target, 'BASE TABLE')
+        except
+        select table_name from information_schema.tables
+        where (table_schema, table_type) = (desired, 'BASE TABLE')
+    loop
+        return next row(format('drop table %I.%I', target, missing_table.table_name), 'drop_table')::alteration;
+    end loop;
 
     for missing_column in
         with missing as (
@@ -69,7 +77,7 @@ begin
         where table_schema = desired
         order by table_name, ordinal_position asc
     loop
-        perform pgdiff.ddl(format(
+        return next row(format(
             'alter table %I.%I add column %I %s %s %s',
             target,
             missing_column.table_name,
@@ -83,27 +91,25 @@ begin
                 then format('default %s', missing_column.column_default)
                 else ''
             end
-        ), dry_run);
+        ), 'add_column')::alteration;
     end loop;
 
-    if not keep_extra then
-        for extra_column in
-            select table_name, column_name
-            from information_schema.columns
-            where table_schema = target
-            except
-            select table_name, column_name
-            from information_schema.columns
-            where table_schema = desired
-        loop
-            perform pgdiff.ddl(format(
-                'alter table %I.%I drop column %I',
-                target,
-                extra_column.table_name,
-                extra_column.column_name
-            ), dry_run);
-        end loop;
-    end if;
+    for extra_column in
+        select table_name, column_name
+        from information_schema.columns
+        where table_schema = target
+        except
+        select table_name, column_name
+        from information_schema.columns
+        where table_schema = desired
+    loop
+        return next row(format(
+            'alter table %I.%I drop column %I',
+            target,
+            extra_column.table_name,
+            extra_column.column_name
+        ), 'drop_column')::alteration;
+    end loop;
 
     for different_column in
         select table_name, column_name, is_nullable, data_type, column_default
@@ -115,45 +121,63 @@ begin
         where table_schema = target
     loop
         if different_column.column_default is not null then
-            perform pgdiff.ddl(format(
+            return next row(format(
                 'alter table %I.%I alter column %I set default %s',
                 target,
                 different_column.table_name,
                 different_column.column_name,
                 different_column.column_default
-            ), dry_run);
+            ), 'alter_column')::alteration;
         else
-            perform pgdiff.ddl(format(
+            return next row(format(
                 'alter table %I.%I alter column %I drop default',
                 target,
                 different_column.table_name,
                 different_column.column_name
-            ), dry_run);
+            ), 'alter_column')::alteration;
         end if;
 
         if different_column.is_nullable then
-            perform pgdiff.ddl(format(
+            return next row(format(
                 'alter table %I.%I alter column %I drop not null',
                 target,
                 different_column.table_name,
                 different_column.column_name
-            ), dry_run);
+            ), 'alter_column')::alteration;
         else
-            perform pgdiff.ddl(format(
+            return next row(format(
                 'alter table %I.%I alter column %I set not null',
                 target,
                 different_column.table_name,
                 different_column.column_name
-            ), dry_run);
+            ), 'alter_column')::alteration;
         end if;
 
-        perform pgdiff.ddl(format(
+        return next row(format(
             'alter table %I.%I alter column %I type %s',
             target,
             different_column.table_name,
             different_column.column_name,
             different_column.data_type
-        ), dry_run);
+        ), 'alter_column')::alteration;
+    end loop;
+end;
+$$;
+
+create procedure pgdiff.migrate(
+    desired text,
+    target text,
+    dry_run bool default true,
+    keep_extra bool default false
+)
+language plpgsql as $$
+declare
+    alteration alteration;
+begin
+    for alteration in
+        select * from pgdiff.alterations(desired, target)
+    loop
+        perform pgdiff.ddl(alteration.ddl, dry_run);
     end loop;
 end;
 $$;
