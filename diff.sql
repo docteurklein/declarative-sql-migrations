1184b9653b7fa39093bbf646099eafcaa51df5aa
Florian Klein <florian.klein@free.fr>
2021-12-02 17:49:07 +0100

    enforce more constraints

--- diff.sql
+++ diff.sql
@@ -44,33 +44,53 @@ and case (value).type
         (value).details ? 'table_name'
     when 'alter table add column' then
         (value).details ? 'schema_name' and
-        (value).details ? 'table_name'
+        (value).details ? 'table_name' and
+        (value).details ? 'column_name' and
+        (value).details ? 'is_nullable' and
+        (value).details ? 'column_default' and
+        (value).details ? 'data_type'
     when 'alter table add constraint' then
         (value).details ? 'schema_name' and
-        (value).details ? 'table_name'
+        (value).details ? 'table_name' and
+        (value).details ? 'constraint_name'
     when 'alter table alter constraint' then
         (value).details ? 'schema_name' and
-        (value).details ? 'table_name'
+        (value).details ? 'table_name' and
+        (value).details ? 'constraint_name' and
+        (value).details ? 'deferrable' and
+        (value).details ? 'deferred'
     when 'alter table drop constraint' then
         (value).details ? 'schema_name' and
-        (value).details ? 'table_name'
+        (value).details ? 'table_name' and
+        (value).details ? 'constraint_name'
     when 'alter column set default' then
         (value).details ? 'schema_name' and
-        (value).details ? 'table_name'
+        (value).details ? 'table_name' and
+        (value).details ? 'column_name' and
+        (value).details ? 'column_default'
     when 'alter column drop default' then
         (value).details ? 'schema_name' and
-        (value).details ? 'table_name'
+        (value).details ? 'table_name' and
+        (value).details ? 'column_name' and
+        (value).details ? 'column_default'
     when 'alter column drop not null' then
         (value).details ? 'schema_name' and
-        (value).details ? 'table_name'
+        (value).details ? 'table_name' and
+        (value).details ? 'column_name' and
+        (value).details ? 'is_nullable'
     when 'alter column set not null' then
         (value).details ? 'schema_name' and
-        (value).details ? 'table_name'
+        (value).details ? 'table_name' and
+        (value).details ? 'column_name' and
+        (value).details ? 'is_nullable'
     when 'alter column type' then
         (value).details ? 'schema_name' and
-        (value).details ? 'table_name'
+        (value).details ? 'table_name' and
+        (value).details ? 'column_name' and
+        (value).details ? 'data_type'
     when 'create index' then
         (value).details ? 'schema_name' and
+        (value).details ? 'index_name' and
         (value).details ? 'table_name'
     when 'drop index' then
         (value).details ? 'schema_name' and
@@ -82,7 +102,8 @@ and case (value).type
         (value).details ? 'cascade'
     when 'drop column' then
         (value).details ? 'schema_name' and
-        (value).details ? 'table_name'
+        (value).details ? 'table_name' and
+        (value).details ? 'column_name'
     else false
 end));
 
