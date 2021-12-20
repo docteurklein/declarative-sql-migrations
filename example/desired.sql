fdd933cd1d7a3e115679bf4d69d5f2bf4cec6c0e
Florian Klein <florian.klein@free.fr>
2021-12-20 15:55:33 +0100

    only custom types, not table types

--- example/desired.sql
+++ example/desired.sql
@@ -7,7 +7,7 @@ set local search_path to desired;
 
 create table test1 (
     test1_id int not null primary key,
-    name text unique not null default 1,
+    name text unique not null check (trim(name) <> '') default 1,
     price int check (price > 0)
 );
 create index test1_name on test1 (name);
--- src/alterations/types.sql
+++ src/alterations/types.sql
@@ -8,7 +8,7 @@ set search_path to pg_catalog
 as $$
 with type_to_drop as (
     select 1, 'drop type',
-    format('drop type %I.%I %s', target, typname,
+    format('drop type %I.%I%s', target, typname,
         case cascade when true then ' cascade' else '' end
     ),
     jsonb_build_object(
@@ -16,7 +16,9 @@ with type_to_drop as (
         'type_name', typname
     )
     from pg_type tt
+    left join pg_class tc on tt.typrelid = tc.oid
     where typnamespace = to_regnamespace(target)::oid
+    and (tc.relkind = 'c' or tc.relkind is null)
     and typtype in ('c', 'e') -- see https://www.postgresql.org/docs/current/catalog-pg-type.html#id-1.10.4.64.4
     and not exists (
         select from pg_type dt
@@ -67,8 +69,10 @@ type_to_create as (
         'type_name', typname
     )
     from pg_type dt
+    left join pg_class dc on dt.typrelid = dc.oid
     where typnamespace = desired::regnamespace::oid
     and typtype in ('c', 'e') -- see https://www.postgresql.org/docs/current/catalog-pg-type.html#id-1.10.4.64.4
+    and (dc.relkind = 'c' or dc.relkind is null)
     and not exists (
         select from pg_type tt
         where typnamespace = to_regnamespace(target)::oid
@@ -80,6 +84,17 @@ type_to_create as (
         ) = array(
             select quote_literal(enumlabel) from pg_enum where enumtypid = tt.oid
             order by enumsortorder
+        )
+        and array(
+            select row(attname, replace(format_type(atttypid, a.atttypmod), format('%I.', desired), format('%I.', target)))
+            from pg_attribute a
+            join pg_class c on dt.typrelid = c.oid
+            and a.attrelid = c.oid
+        ) = array(
+            select row(attname, replace(format_type(atttypid, a.atttypmod), format('%I.', desired), format('%I.', target)))
+            from pg_attribute a
+            join pg_class c on tt.typrelid = c.oid
+            and a.attrelid = c.oid
         )
     )
 )
