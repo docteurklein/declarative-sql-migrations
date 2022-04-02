fbe5b70ec274e389dd1796187bc658efd640528a
Florian Klein <florian.klein@free.fr>
2022-04-02 21:01:09 +0200

    validate record

--- src/violations.sql
+++ src/violations.sql
@@ -1,24 +1,22 @@
 drop type if exists pgdiff.status cascade;
-create type pgdiff.status as enum ('valid', 'invalid check', 'null', 'unknown');
+create type pgdiff.status as enum ('valid', 'invalid', 'null');
 
-create or replace function pgdiff.is_valid(key text, value anyelement, type_ text, coid oid)
-returns pgdiff.status
+create or replace function pgdiff.is_valid(schema text, tbl text, key text, record jsonb, coid oid)
+returns bool
 language plpgsql strict volatile
 set search_path to pgdiff
 as $$
+declare
+    result record;
 begin
     execute format(
-	'with _row(%I) as (select $1::%I) select from _row where %s',
-	key,
-	type_,
+	'select exists(select from jsonb_populate_record(null::%I.%I, $1) where %s) e',
+	schema,
+	tbl,
 	trim(leading 'CHECK ' from pg_get_constraintdef(coid))
-    ) using value;
+    ) strict into result using record;
 
-    case when found then
-	return 'valid'::status;
-    else
-	return 'invalid check'::status;
-    end case;
+    return result.e;
 end;
 $$;
 
@@ -35,25 +33,32 @@ language sql strict stable
 set search_path to pgdiff, pg_catalog
 as $$
     select
-	e.key,
+	a.attname,
 	c.conname,
 	pg_get_constraintdef(c.oid),
 	coalesce(
-	    -- case when a.attname is null then 'unknown'::status else null end,
-	    -- case when a.attnotnull then
-	    --     (case when value is null then 'null'::status else null end)
-	    --     else null
-	    -- end,
-	    is_valid(e.key, e.value, format_type(t.oid, null), c.oid)
+	    case when a.attnotnull then
+	        (case when value is null then 'null'::status else null end)
+	        else null
+	    end,
+	    case is_valid(
+		-- string_agg(quote_ident(key), ', ') from jsonb_object_keys(record),
+		'desired',
+		'test1',
+		e.key,
+		record,
+		c.oid
+	    ) when true then 'valid'::status else 'invalid'::status end
 	)
-    from jsonb_each_text(record) e
-    left join pg_attribute a
+    from pg_attribute a
+    left join jsonb_each_text(record) e
         on a.attname = e.key
-        and a.attrelid = toid
     left join pg_constraint c
         on a.attnum = any(c.conkey) 
 	and c.conrelid = toid
 	and c.contype in ('c') -- f, p, u
     left join pg_type t
         on a.atttypid = t.oid
+    where a.attrelid = toid
+    and a.attnum > 0
 $$;
--- test/violations.sql
+++ test/violations.sql
@@ -11,9 +11,9 @@ begin
     create table test1 (
 	id int not null,
 	content text not null check (length(content) > 0),
-	age int check (age > 18)
-	-- work int not null,
-	-- check ((age >= 18 and work < 2 ) or (age < 18 and work > 2))
+	age int check (age > 18),
+	work int not null,
+	check ((age >= 18 and work < 2 ) or (age < 18 and work > 2))
     );
 
     -- assert count(_log(v, null)) > 0 from pgdiff.violations('{"content": ""}'::jsonb,'desired.test1'::regclass::oid) v;
